433e631b3e258dcf74cc021502145be9427c45dc
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-05-27 15:32:11 +0800

    complete basic crud features

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -35,4 +35,26 @@ defmodule Discuss.TopicController do
     render conn, "edit.html", changeset: changeset, topic: topic
   end
 
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn = put_flash(conn, :error, "Error in form")
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
 end
