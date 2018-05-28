efbc297db3b4b71ab30d5bb49d31553e618dbad4
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-05-29 06:49:38 +0800

    require auth when editing/adding

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,15 +3,17 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug(Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete])
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
-    render conn, "index.html", topics: topics
+    render(conn, "index.html", topics: topics)
   end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
@@ -22,17 +24,17 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
         conn = put_flash(conn, :error, "Error in form")
-        render conn, "new.html", changeset: changeset
-
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
   def edit(conn, %{"id" => topic_id}) do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
-    render conn, "edit.html", changeset: changeset, topic: topic
+    render(conn, "edit.html", changeset: changeset, topic: topic)
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
@@ -44,14 +46,15 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:info, "Topic updated")
         |> redirect(to: topic_path(conn, :index))
+
       {:error, changeset} ->
         conn = put_flash(conn, :error, "Error in form")
-        render conn, "edit.html", changeset: changeset, topic: old_topic
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
   end
 
   def delete(conn, %{"id" => topic_id}) do
-    Repo.get!(Topic, topic_id) |> Repo.delete!
+    Repo.get!(Topic, topic_id) |> Repo.delete!()
 
     conn
     |> put_flash(:info, "Topic Deleted")
